eb2aed083cc0a924213d845cdf4eee23e9fbd35a
SAMA CASA <L20270788@tuxtla.tecnm.mx>
2026-05-06 23:39:47 -0600

    Eliminando ruta gastos-fijos

--- routes/web.php
+++ routes/web.php
@@ -7,20 +7,19 @@ Route::view('/', 'welcome');
 
 // El nuevo bloque de seguridad de Breeze para Alberto
 Route::middleware(['auth', 'verified'])->group(function () {
-    
+
     // Cambiamos el Route::view de Breeze por tu componente Volt
     Volt::route('/dashboard', 'dashboard')->name('dashboard');
-    
+
     // Tus rutas rescatadas
     Volt::route('/registrar', 'registrar-movimiento')->name('registrar');
     Volt::route('/historial', 'lista-movimientos')->name('historial');
     Volt::route('/configuracion', 'gestion-cuentas')->name('configuracion');
     Volt::route('/resumen', 'resumen-mensual')->name('resumen');
-    Volt::route('/gastos-fijos', 'gastos-fijos')->name('gastos-fijos');
 
     // La ruta de perfil que Breeze sí debe mantener
     Route::view('/profile', 'profile')->name('profile');
 });
 
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
